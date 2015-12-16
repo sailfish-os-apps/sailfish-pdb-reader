811cd9eda29f89c08ba778d17ebcd17a7fda4a1d
Dominik Chrástecký <dominik@chrastecky.cz>
2015-12-16 14:04:57 +0100

    Adding license

--- translations/harbour-pdb-reader-cs_CZ.ts
+++ translations/harbour-pdb-reader-cs_CZ.ts
@@ -4,6 +4,7 @@
 <context>
     <name>CoverPage</name>
     <message>
+        <location filename="../qml/cover/CoverPage.qml" line="38"/>
         <source>My Cover</source>
         <translation type="unfinished"></translation>
     </message>
@@ -11,14 +12,17 @@
 <context>
     <name>FirstPage</name>
     <message>
+        <location filename="../qml/pages/FirstPage.qml" line="45"/>
         <source>Show Page 2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../qml/pages/FirstPage.qml" line="61"/>
         <source>UI Template</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../qml/pages/FirstPage.qml" line="65"/>
         <source>Hello Sailors</source>
         <translation type="unfinished"></translation>
     </message>
@@ -26,10 +30,12 @@
 <context>
     <name>SecondPage</name>
     <message>
+        <location filename="../qml/pages/SecondPage.qml" line="42"/>
         <source>Nested Page</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../qml/pages/SecondPage.qml" line="49"/>
         <source>Item</source>
         <translation type="unfinished"></translation>
     </message>
